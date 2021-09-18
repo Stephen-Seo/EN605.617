a9e194063970059e292b891d13cc96575eff2204
Stephen Seo <seo.disparate@gmail.com>
2021-09-18 14:37:53 +0900

    Impl the four required mathematical fns

--- module3/assignment.cu
+++ module3/assignment.cu
@@ -1,22 +1,35 @@
 //Based on the work of Andrew Krepps
 #include <stdio.h>
-
-#define ARRAY_SIZE 256
-#define ARRAY_SIZE_IN_BYTES (sizeof(unsigned int) * ARRAY_SIZE)
+#include <stdlib.h>
+#include <time.h>
 
 __global__
-void do_work(unsigned int *x, unsigned int *y, unsigned int *out) {
+void add(unsigned int *x, unsigned int *y, unsigned int *out) {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
     out[thread_idx] = x[thread_idx] + y[thread_idx];
 }
 
-unsigned int hostX[ARRAY_SIZE];
-unsigned int hostY[ARRAY_SIZE];
-unsigned int hostOut[ARRAY_SIZE];
+__global__
+void subtract(unsigned int *x, unsigned int *y, unsigned int *out) {
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    out[thread_idx] = x[thread_idx] - y[thread_idx];
+}
+
+__global__
+void mult(unsigned int *x, unsigned int *y, unsigned int *out) {
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    out[thread_idx] = x[thread_idx] * y[thread_idx];
+}
+
+__global__
+void mod(unsigned int *x, unsigned int *y, unsigned int *out) {
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    out[thread_idx] = x[thread_idx] % y[thread_idx];
+}
 
 int main(int argc, char** argv) {
 	// read command line arguments
-	int totalThreads = ARRAY_SIZE;
+	int totalThreads = 512;
 	int blockSize = 256;
 	
 	if (argc >= 2) {
@@ -35,16 +48,26 @@ int main(int argc, char** argv) {
 		++numBlocks;
 		totalThreads = numBlocks*blockSize;
 		
-		printf("Warning: Total thread count is not evenly divisible by the block size\n");
-		printf("The total number of threads will be rounded up to %d\n", totalThreads);
+		printf("Warning: Total thread count is not evenly divisible by the "
+               "block size\n");
+		printf("The total number of threads will be rounded up to %d\n",
+                totalThreads);
 	}
     printf("totalThreads == %3u, numBlocks == %3u, blockSize == %3u\n",
         totalThreads, numBlocks, blockSize);
 
+    unsigned int *hostX =
+        (unsigned int*)malloc(sizeof(unsigned int) * totalThreads);
+    unsigned int *hostY =
+        (unsigned int*)malloc(sizeof(unsigned int) * totalThreads);
+    unsigned int *hostOut =
+        (unsigned int*)malloc(sizeof(unsigned int) * totalThreads);
+
     printf("Setting host values...\n");
-    for(unsigned int i = 0; i < ARRAY_SIZE; ++i) {
+    srand(time(NULL));
+    for(unsigned int i = 0; i < totalThreads; ++i) {
         hostX[i] = i;
-        hostY[i] = i + i;
+        hostY[i] = rand() % 4;
         hostOut[i] = 0;
     }
 
@@ -53,26 +76,29 @@ int main(int argc, char** argv) {
     unsigned int *out;
 
     printf("cudaMalloc...\n");
-    cudaMalloc((void**)&x, ARRAY_SIZE_IN_BYTES);
-    cudaMalloc((void**)&y, ARRAY_SIZE_IN_BYTES);
-    cudaMalloc((void**)&out, ARRAY_SIZE_IN_BYTES);
+    cudaMalloc((void**)&x, totalThreads * sizeof(unsigned int));
+    cudaMalloc((void**)&y, totalThreads * sizeof(unsigned int));
+    cudaMalloc((void**)&out, totalThreads * sizeof(unsigned int));
 
     printf("cudaMemcpy...\n");
-    cudaMemcpy(x, hostX, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
-    cudaMemcpy(y, hostY, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+    cudaMemcpy(x, hostX, totalThreads * sizeof(unsigned int),
+            cudaMemcpyHostToDevice);
+    cudaMemcpy(y, hostY, totalThreads * sizeof(unsigned int),
+            cudaMemcpyHostToDevice);
 
-    printf("Doing work...\n");
-    do_work<<<numBlocks, totalThreads>>>(x, y, out);
+    printf("Executing \"add\"...\n");
+    add<<<numBlocks, totalThreads>>>(x, y, out);
 
     printf("Copying result to host...\n");
-    cudaMemcpy(hostOut, out, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostOut, out, totalThreads * sizeof(unsigned int),
+            cudaMemcpyDeviceToHost);
 
     printf("Freeing device memory...\n");
     cudaFree(x);
     cudaFree(y);
     cudaFree(out);
 
-    for(unsigned int j = 0; j < ARRAY_SIZE / 4; ++j) {
+    for(unsigned int j = 0; j < totalThreads / 4; ++j) {
         printf("%3u: %4u\t%3u: %4u\t%3u: %4u\t %3u: %4u\n",
             j*4, hostOut[j*4],
             1+j*4,hostOut[1+j*4],
@@ -80,5 +106,12 @@ int main(int argc, char** argv) {
             3+j*4,hostOut[3+j*4]);
     }
 
+    printf("Freeing host memory...\n");
+    free(hostX);
+    free(hostY);
+    free(hostOut);
+
     return EXIT_SUCCESS;
 }
+
+// vim: cindent: ts=4: sw=4: et
