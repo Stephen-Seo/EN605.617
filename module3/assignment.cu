ce4f4561a664309bd0f51aa9d69aa19b9eb62cc4
Stephen Seo <seo.disparate@gmail.com>
2021-09-19 16:11:32 +0900

    Improve non-branching kernel's algorithm

--- module3/assignment.cu
+++ module3/assignment.cu
@@ -72,30 +72,30 @@ __global__
 void non_branching_mathexpressions(int *x, int *y, int *out) {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
     const unsigned int sidx = thread_idx % 4;
-    // i=0  (3 ^ i) & 1 >> i = 1
-    // i=1  (3 ^ i) & 1 >> i = 0
-    // i=2  (3 ^ i) & 1 >> i = 0
-    // i=3  (3 ^ i) & 1 >> i = 0
+    // i=0  (i & 1) >> (3 - i) = 0
+    // i=1  (i & 1) >> (3 - i) = 0
+    // i=2  (i & 1) >> (3 - i) = 0
+    // i=3  (i & 1) >> (3 - i) = 1
     //
-    // i=0  ((3 >> i) & 1) * i = 0
-    // i=1  ((3 >> i) & 1) * i = 1
-    // i=2  ((3 >> i) & 1) * i = 0
-    // i=3  ((3 >> i) & 1) * i = 0
+    // i=0  ((3-i) & 1) >> i = 1
+    // i=1  ((3-i) & 1) >> i = 0
+    // i=2  ((3-i) & 1) >> i = 0
+    // i=3  ((3-i) & 1) >> i = 0
     //
-    // i=0  (3 ^ (3-i)) & 1 >> (3-i) = 0
-    // i=1  (3 ^ (3-i)) & 1 >> (3-i) = 0
-    // i=2  (3 ^ (3-i)) & 1 >> (3-i) = 0
-    // i=3  (3 ^ (3-i)) & 1 >> (3-i) = 1
+    // i=0  (3 >> i) * i = 0
+    // i=1  (3 >> i) * i = 1
+    // i=2  (3 >> i) * i = 0
+    // i=3  (3 >> i) * i = 0
     //
-    // i=0  ((3 >> (3-i)) & 1) * (3-i) = 0
-    // i=1  ((3 >> (3-i)) & 1) * (3-i) = 0
-    // i=2  ((3 >> (3-i)) & 1) * (3-i) = 1
-    // i=3  ((3 >> (3-i)) & 1) * (3-i) = 0
+    // i=0  (3 >> (3-i)) * (3-i) = 0
+    // i=1  (3 >> (3-i)) * (3-i) = 0
+    // i=2  (3 >> (3-i)) * (3-i) = 1
+    // i=3  (3 >> (3-i)) * (3-i) = 0
     out[thread_idx] =
-        (((3 ^ sidx) & 1) >> sidx)         * (x[thread_idx] + y[thread_idx])
-      + (((3 >> sidx) & 1) * sidx)         * (x[thread_idx] - y[thread_idx])
-      + (((3 >> (3-sidx)) & 1) * (3-sidx)) * (x[thread_idx] * y[thread_idx])
-      + (((3 ^ (3-sidx)) & 1) >> (3-sidx)) * (x[thread_idx] % y[thread_idx]);
+        (((3 - sidx) & 1) >> sidx)         * (x[thread_idx] + y[thread_idx])
+      + ((3 >> sidx) * sidx)               * (x[thread_idx] - y[thread_idx])
+      + ((3 >> (3-sidx)) * (3-sidx))       * (x[thread_idx] * y[thread_idx])
+      + ((sidx & 1) >> (3 - sidx))         * (x[thread_idx] % y[thread_idx]);
 }
 
 //__global__
