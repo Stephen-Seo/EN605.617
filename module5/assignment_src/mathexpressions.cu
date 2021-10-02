b7ad6b6bc8688d87b00e097ff3b2ab1a05f5871c
Stephen Seo <seo.disparate@gmail.com>
2021-10-02 15:19:32 +0900

    M05 Minor fixes/comment-additions

--- module5/assignment_src/mathexpressions.cu
+++ module5/assignment_src/mathexpressions.cu
@@ -10,7 +10,6 @@ void copy_to_shared(const int *x,
                     const unsigned int thread_idx) {
     x_shared[threadIdx.x] = x[thread_idx];
     y_shared[threadIdx.x] = y[thread_idx];
-    __syncthreads();
 }
 
 __device__
@@ -30,6 +29,9 @@ void mathexpressions_shared(const int *x, const int *y, int *out) {
 
     copy_to_shared(x, y, x_shared, y_shared, thread_idx);
 
+    // sync here to ensure shared data is fully populated
+    __syncthreads();
+
     const unsigned int subidx = thread_idx % 4;
     if (subidx == 0) {
         out_shared[threadIdx.x] = x_shared[threadIdx.x] + y_shared[threadIdx.x];
@@ -40,6 +42,8 @@ void mathexpressions_shared(const int *x, const int *y, int *out) {
     } else {
         out_shared[threadIdx.x] = x_shared[threadIdx.x] % y_shared[threadIdx.x];
     }
+
+    // sync here to ensure out_shared is fully populated
     __syncthreads();
 
     copy_from_shared_out(out, out_shared, thread_idx);
