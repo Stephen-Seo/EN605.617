29f6dd90aaae26958b509328017329ff44a76739
Stephen Seo <seo.disparate@gmail.com>
2021-10-02 14:55:03 +0900

    M05 Add run.sh

---
+++ module5/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function on_exit() {
+    #make clean || true
+    popd &>/dev/null || true
+}
+
+trap on_exit EXIT
+
+set -e
+
+pushd "$(dirname "$0")" &>/dev/null
+
+make assignment.exe
+
+./assignment.exe "$@"
