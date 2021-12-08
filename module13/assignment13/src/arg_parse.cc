352765fa30758c2e6da1f7bceaca7425c3502ae8
Stephen Seo <seo.disparate@gmail.com>
2021-12-08 14:47:05 +0900

    Init Module13 assignment code

---
+++ module13/assignment13/src/arg_parse.cc
@@ -0,0 +1,12 @@
+#include "arg_parse.h"
+
+Args::Args() :
+  input_filename()
+{}
+
+bool Args::ParseArgs(int argc, char **argv) {
+  return false;
+}
+
+void Args::PrintUsage() {
+}
---
+++ module13/assignment13/src/arg_parse.h
@@ -0,0 +1,17 @@
+#ifndef IGPUP_MODULE_13_EVENTS_COMMAND_QUEUES_ARG_PARSE_H_
+#define IGPUP_MODULE_13_EVENTS_COMMAND_QUEUES_ARG_PARSE_H_
+
+#include <string>
+
+struct Args {
+  Args();
+
+  /// Returns true if help was printed
+  bool ParseArgs(int argc, char **argv);
+
+  static void PrintUsage();
+
+  std::string input_filename;
+};
+
+#endif
